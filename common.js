36d4e716420654d1accc0d7a40988cdc5433d185
nathasjafink <nathasja.nielsen@gmail.com>
2023-12-04 17:16:49 +0100

    added fetch in the map

--- common.js
+++ common.js
@@ -23,7 +23,20 @@ function fetchCafesAndDisplay () {
 function fetchDataAndDisplayMap () {
     const map = L.map('map').setView([55.6761,12.5683],13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tilelayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
+
+    fetch("http://localhost:3000/cafe")
+        .then(response=> response.json())
+        .then(location => {
+            location.forEach(location => {
+               const marker = L.marker([cafe.latitude, cafe.longitude])
+                   .addTo(map)
+                   .bindPopup(`<b>${cafe.cafe_name}</b><br>${cafe.address}`)
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching cafe data",error);
+        });
 }
