dc6362eedf51f8fabfa2faee336b6b099ecbcfff
BirkLauritzen <birklauritzen@gmail.com>
2023-12-08 09:42:48 +0100

    Update cafe_database.sql

--- cafe_database.sql
+++ cafe_database.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS cafes;
 /* Create tables */
 create table cafes
 (
-    cafe_id      int           not null
+    cafe_id      int auto_increment   not null
         primary key,
     cafe_name    varchar(100)  null,
     descriptions text          null,
@@ -26,10 +26,13 @@ create table users
         primary key,
     firstName       varchar(100) null,
     lastName        varchar(100) null,
+    email           varchar(100) not null,
     username        varchar(255) not null,
     hashed_password char(60)     not null,
     constraint username
-        unique (username)
+        unique (username),
+    constraint email
+        unique (email)
 )
     engine = InnoDB;
 
@@ -73,17 +76,17 @@ VALUES
     ('16', 'Riccos', NULL, 'Istedgade 119, 1650 Copenhagen', '3', '12.548264', '55.667615');
 
 /* Insert data into users */
-INSERT INTO users (users_id, firstName, lastName, username, hashed_password) VALUES
-                                                                                 (1, 'John', 'Doe', 'johndoe', 'hashedpassword1'),
-                                                                                 (2, 'Alice', 'Smith', 'alicesmith', 'hashedpassword2'),
-                                                                                 (3, 'Bob', 'Johnson', 'bobjohnson', 'hashedpassword3'),
-                                                                                 (4, 'Emily', 'Williams', 'emilywilliams', 'hashedpassword4'),
-                                                                                 (5, 'Michael', 'Jones', 'michaeljones', 'hashedpassword5'),
-                                                                                 (6, 'Sophia', 'Brown', 'sophiabrown', 'hashedpassword6'),
-                                                                                 (7, 'Daniel', 'Taylor', 'danieltaylor', 'hashedpassword7'),
-                                                                                 (8, 'Olivia', 'Miller', 'oliviamiller', 'hashedpassword8'),
-                                                                                 (9, 'David', 'Anderson', 'davidanderson', 'hashedpassword9'),
-                                                                                 (10, 'Emma', 'Davis', 'emmadavis', 'hashedpassword10');
+INSERT INTO users (users_id, firstName, lastName, email, username, hashed_password) VALUES
+                                                                                 (1, 'John', 'Doe', 'john.doe@example.com', 'johndoe', 'hashedpassword1'),
+                                                                                 (2, 'Alice', 'Smith', 'alice.smith@example.com', 'alicesmith', 'hashedpassword2'),
+                                                                                 (3, 'Bob', 'Johnson', 'bob.johnson@example.com', 'bobjohnson', 'hashedpassword3'),
+                                                                                 (4, 'Emily', 'Williams', 'emily.williams@example.com', 'emilywilliams', 'hashedpassword4'),
+                                                                                 (5, 'Michael', 'Jones', 'michael.jones@example.com', 'michaeljones', 'hashedpassword5'),
+                                                                                 (6, 'Sophia', 'Brown', 'sophia.brown@example.com', 'sophiabrown', 'hashedpassword6'),
+                                                                                 (7, 'Daniel', 'Taylor', 'daniel.taylor@example.com', 'danieltaylor', 'hashedpassword7'),
+                                                                                 (8, 'Olivia', 'Miller', 'olivia.miller@example.com', 'oliviamiller', 'hashedpassword8'),
+                                                                                 (9, 'David', 'Anderson', 'david.anderson@example.com', 'davidanderson', 'hashedpassword9'),
+                                                                                 (10, 'Emma', 'Davis', 'emma.davis@example.com', 'emmadavis', 'hashedpassword10');
 
 /* Insert data into favorites */
 INSERT INTO favorites (cafe_id, favorite_cafe_name, users_id) VALUES
