3d6f4bf1e4190911b3d7155e3f2fe701984d4ef7
nathasjafink <nathasja.nielsen@gmail.com>
2023-12-11 09:46:39 +0100

    added new cafe feature in

---
+++ public/addcafepage/new-cafe.js
@@ -0,0 +1,20 @@
+const btnForCafe = document.querySelector('#btn-for-cafe');
+btnForCafe.addEventListener('click', function () {
+    const cafeNameInput = document.querySelector('#cafe-name').value;
+    const cafeAddressInput = document.querySelector('#cafe-address').value;
+    const cityInput = document.querySelector("#city").value;
+    const postalCodeInput = document.querySelector("#postal-code").value;
+
+    console.log(cafeNameInput, cafeAddressInput, cityInput, postalCodeInput);
+
+    fetchlatandlong(cafeNameInput, cafeAddressInput, cityInput, postalCodeInput).then(coordinates => {
+        // Do something with the coordinates if needed
+        console.log("Coordinates:", coordinates);
+    })
+        .catch(error => {
+            console.error("Error fetching coordinates:", error);
+        });
+
+
+    getCoordinatesInDb(cafeNameInput, cafeAddressInput, cityInput, postalCodeInput);
+});
