e36d27bb9d1f3e54aba44d590d46764ea06018f1
BirkLauritzen <birklauritzen@gmail.com>
2023-12-13 01:13:04 +0100

    Updated camelcase to underscore

--- public/user/user_account.js
+++ public/user/user_account.js
@@ -1,16 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/api/user-info', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('userToken') // or your method of sending the token
+async function fetchUserInfo() {
+    try {
+        const response = await fetch('/api/user-info', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('userToken') // or your method of sending the token
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Assuming data contains user details
-            document.querySelector('.user-data p:nth-child(1)').textContent = 'Name: ' + data.name;
-            document.querySelector('.user-data p:nth-child(2)').textContent = 'Email: ' + data.email;
-            document.querySelector('.user-data p:nth-child(3)').textContent = 'Address: ' + data.address;
-        })
-        .catch(error => console.error('Error:', error));
-});
+
+        const data = await response.json();
+
+        // Assuming data contains user details
+        document.querySelector('.user-data p:nth-child(1)').textContent = `Name: ${data.first_name} ${data.last_name}`;
+        document.querySelector('.user-data p:nth-child(2)').textContent = `Email: ${data.email}`;
+        document.querySelector('.user-data p:nth-child(3)').textContent = `Address: ${data.address}`;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchUserInfo);
