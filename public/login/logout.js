fcb738efc82098de03978174d8261f9d76dc289a
BirkLauritzen <birklauritzen@gmail.com>
2023-12-14 22:27:51 +0100

    Logout button

---
+++ public/login/logout.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const logoutButton = document.getElementById('logoutButton');
+    if (logoutButton) {
+        logoutButton.addEventListener('click', () => {
+            // Clear any stored data like sessionId or userInfo
+            localStorage.removeItem('sessionId');
+            localStorage.removeItem('userInfo');
+
+            // Redirect to the login page
+            window.location.href = '/index.html';
+        });
+    }
+});
