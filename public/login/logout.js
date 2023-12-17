b99897cf3f3d368627de322df634e7cdf9d9c31c
BirkLauritzen <birklauritzen@gmail.com>
2023-12-17 21:43:58 +0100

    Update logout.js

--- public/login/logout.js
+++ public/login/logout.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logoutButton');
     if (logoutButton) {
         logoutButton.addEventListener('click', () => {
-            // Clear any stored data like sessionId or userInfo
+            // Retrieve the userId from localStorage
+            const userId = localStorage.getItem('sessionId');
+
+            // Clear any stored data like sessionId, userInfo, and userFavorites
+            if (userId) {
+                localStorage.removeItem(`userFavorites-${userId}`);
+            }
             localStorage.removeItem('sessionId');
             localStorage.removeItem('userInfo');
-            localStorage.removeItem(`userFavorites-${userId}`);
 
             // Redirect to the login page
             window.location.href = '/index.html';
